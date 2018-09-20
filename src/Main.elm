129446c53e916389d01968028ed8444292e64766
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-09-20 15:33:11 +0200

    Create a Browser.element

--- src/Main.elm
+++ src/Main.elm
@@ -1,32 +1,101 @@
 module Main exposing (main)
 
+import Browser
 import Html
-import Plane exposing (boundingbox, boxToSvg, fromPair, pointToSvg)
-import Quadtree exposing (quadtreeToSvg)
+import Html.Attributes as Attribute
+import Plane exposing (Box, Point, boundingbox, boxToSvg, fromPair, pointToSvg)
+import Quadtree exposing (Quadtree, quadtreeToSvg)
 import Svg
-import Svg.Attributes as Attribute
+import Svg.Attributes as SvgAttribute
 
 
+main : Program () Model Message
 main =
     let
-        points =
-            [ ( 0, 0 ), ( 37, 51 ), ( 1, 2 ), ( 2, 3 ), ( 3, 5 ), ( 5, 8 ) ]
-                |> List.map fromPair
-
         box =
-            Just (Plane.box 0 0 51 51)
-
-        quadtree =
-            Maybe.map (\b -> Quadtree.for b points) box
-
-        svg =
-            quadtree
-                |> Maybe.map (\t -> quadtreeToSvg (pointToSvg identity) t)
-                |> Maybe.withDefault []
+            Plane.box 0 0 100 100
     in
-    Svg.svg
-        [ Attribute.width "640"
-        , Attribute.height "640"
-        , Attribute.viewBox "0 0 51 51"
+    Browser.element
+        { init = \_ -> ( emptyModel 10 box, Cmd.none )
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+
+-- Model
+
+
+type alias Model =
+    { numberOfPoints : Int
+    , box : Plane.Box Float
+    , quadtree : Quadtree (Point Float)
+    }
+
+
+emptyModel : Int -> Box Float -> Model
+emptyModel numberOfPoints box =
+    { numberOfPoints = numberOfPoints
+    , box = box
+    , quadtree = Quadtree.for box []
+    }
+
+
+
+-- Update
+
+
+type Message
+    = GeneratePoints
+
+
+update : Message -> Model -> ( Model, Cmd Message )
+update message model =
+    ( model, Cmd.none )
+
+
+
+-- View
+
+
+view : Model -> Html.Html Message
+view model =
+    let
+        svg =
+            model.quadtree
+                |> quadtreeToSvg (pointToSvg identity)
+    in
+    Html.div []
+        [ Html.div [ Attribute.class "control" ]
+            [ Html.button [] [ Html.text "generate" ]
+            ]
+        , Svg.svg
+            [ SvgAttribute.width "640"
+            , SvgAttribute.height "640"
+            , SvgAttribute.viewBox "0 0 100 100"
+            ]
+            svg
         ]
-        svg
+
+
+
+-- let
+--     points =
+--         [ ( 0, 0 ), ( 37, 51 ), ( 1, 2 ), ( 2, 3 ), ( 3, 5 ), ( 5, 8 ) ]
+--             |> List.map fromPair
+--     box =
+--         Just (Plane.box 0 0 51 51)
+--     quadtree =
+--         Maybe.map (\b -> Quadtree.for b points) box
+--     svg =
+--         quadtree
+--             |> Maybe.map (\t -> quadtreeToSvg (pointToSvg identity) t)
+--             |> Maybe.withDefault []
+-- in
+-- Svg.svg
+--     [ Attribute.width "640"
+--     , Attribute.height "640"
+--     , Attribute.viewBox "0 0 51 51"
+--     ]
+--     svg
